1365a4100a26b56c71bbabc72979f524e52a5492
Alifoxy <nightfoxy666@gmail.com>
2024-10-29 12:21:22 +0300

    lesson 1

--- src/database/entities/enums/table-name.enum.ts
+++ src/database/entities/enums/table-name.enum.ts
@@ -1,6 +1,8 @@
 export enum TableNameEnum {
   ARTICLES = 'articles',
+  COMMENTS = 'comments',
   LIKES = 'likes',
+  FOLLOW = 'follow',
   REFRESH_TOKENS = 'refresh_tokens',
   TAGS = 'tags',
   USERS = 'users',
